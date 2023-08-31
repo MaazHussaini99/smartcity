caf2ea3b5c6a9935efad2482c2b511472fa36500
KMSDax <64090073+KMSDax@users.noreply.github.com>
2023-08-31 09:39:32 -0400

    Login And Signup Page WIP DBConn moved to seperate file

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -20,37 +20,8 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-
-        Connection connection = null;
-        try {
-            // below two lines are used for connectivity.
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/smartcity",
-                    "root", "");
-
-            // mydb is database
-            // mydbuser is name of database
-            // mydbuser is password of database
-
-            Statement statement;
-            statement = connection.createStatement();
-            ResultSet resultSet;
-            resultSet = statement.executeQuery(
-                    "select * from bank");
-            int code;
-            String title;
-            while (resultSet.next()) {
-                title = resultSet.getString("bank_name").trim();
-                System.out.println(" Title : " + title);
-            }
-            resultSet.close();
-            statement.close();
-            connection.close();
-        }
-        catch (Exception exception) {
-            System.out.println(exception);
-        }
+        DBConn newDB = new DBConn();
+        newDB.DBConn();
     }
 
     public static void main(String[] args) {
