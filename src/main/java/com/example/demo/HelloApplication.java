9c901b98b2d45b5938198ef7d6dd2f1b8b05a0bc
Maaz Hussaini <maazhussaini@outlook.com>
2023-08-31 11:48:20 -0400

    maaz commit

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -4,10 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.sql.*;
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
+
 import java.sql.SQLException;
 
 import java.io.IOException;
@@ -20,6 +17,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        News.getNews();
     }
 
     public static void main(String[] args) {
