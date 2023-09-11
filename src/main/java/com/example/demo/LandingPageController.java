71e226dbd291f700870922ea22fba425f3edfb07
KMSDax <64090073+KMSDax@users.noreply.github.com>
2023-09-11 13:05:36 -0400

    Merged master branch into Owen's branch

--- src/main/java/com/example/demo/LandingPageController.java
+++ src/main/java/com/example/demo/LandingPageController.java
@@ -3,35 +3,32 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.AnchorPane;
 
-import java.io.FileNotFoundException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class LandingPageController implements Initializable {
 
     @FXML
-    private ListView<String> newsList;
+    private ListView<News> newsList;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
-            List<News> newsList = new ArrayList<>();
-            newsList = News.getNews();
-            for(News news : newsList){
+            ObservableList<News> names = FXCollections.observableArrayList(News.getNews());
+            News[] newsArray = new News[names.size()];
+            names.toArray(newsArray);
+
+            for (News news : newsArray) {
                 System.out.println(news.getTitle());
                 System.out.println(news.getDescription());
                 System.out.println(news.getUrl());
                 System.out.println(news.getImg_url());
                 System.out.println();
             }
-//            ObservableList<String> names = FXCollections.observableArrayList(News.getNewsTitles());
-//            newsList.setItems(names);
+
+            newsList.setItems(names);
         } catch (Exception e) {
             e.printStackTrace();
         }
