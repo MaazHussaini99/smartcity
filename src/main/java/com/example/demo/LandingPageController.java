b2a7fe1158ca5214dcad83908d441eff56fa7b56
KMSDax <64090073+KMSDax@users.noreply.github.com>
2023-09-19 08:53:14 -0400

    morning updates before starting

--- src/main/java/com/example/demo/LandingPageController.java
+++ src/main/java/com/example/demo/LandingPageController.java
@@ -301,21 +301,22 @@ public class LandingPageController extends HotelBookingController implements Ini
         Pane userDataPane = new Pane();
 
         // Populate the user data Pane with user-specific content
-        Text userDataText = new Text("First name: " + User.getInstance().getFirstName()
-                + "\nLast name: " + User.getInstance().getLastName()
-                + "\nStreet address: " + User.getInstance().getStreetAddress()
-                + "\nCity: " + User.getInstance().getCity()
-                + "\nZipcode: " + User.getInstance().getZipcode()
-                + "\nState: " + User.getInstance().getState()
+        Text userDataText = new Text("Name: " + User.getInstance().getFirstName() + " " + User.getInstance().getLastName()
                 + "\nEmail: " + User.getInstance().getEmail()
                 + "\nPhone number: " + User.getInstance().getPhoneNumber()
-                + "\n\nUse role ID to check if a user is an admin or not.\n1 = User, 2 = Admin"
-                + "\nUser role ID: " + User.getInstance().getRoleID()); // Replace with your user data components
+                + "\nUser role ID: " + User.getInstance().getRoleID() // Replace with your user data components
+                + "\nAddress: \n" + User.getInstance().getStreetAddress()
+                + "\n" + User.getInstance().getCity()
+                + ", " + User.getInstance().getZipcode()
+                + ", " + User.getInstance().getState());
         userDataText.setLayoutX(10);
         userDataText.setLayoutY(10);
         adminButton = new Button("Add new Admin"); // Replace with your user data components
         adminButton.setLayoutX(10);
         adminButton.setLayoutY(200);
+
+        // Add an event handler to the button
+        adminButton.setOnAction(event -> loadAdminFXML());
 
         newCard = new Button("Add a New Payment Method"); // Replace with your user data components
         newCard.setLayoutX(10);
@@ -343,6 +344,19 @@ public class LandingPageController extends HotelBookingController implements Ini
 
             // Get the current stage and set the new scene
             Stage stage = (Stage) newCard.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void loadAdminFXML() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("adminpanel.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) adminButton.getScene().getWindow();
             stage.setScene(scene);
             stage.show();
             stage.centerOnScreen();
